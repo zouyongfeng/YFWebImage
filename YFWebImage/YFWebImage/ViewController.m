d90d5f4d7f280e71a163e522f53c1a8aed6da259
zouyf <zouyf@mizlicai.com>
2017-09-09 09:39:44 +0800

    commit

--- YFWebImage/YFWebImage/ViewController.m
+++ YFWebImage/YFWebImage/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "UIImageView+YFWebImage.h"
+
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIImageView *imageView;
 
 @end
 
@@ -17,6 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.imageView setImageWithURL:[NSURL URLWithString:@"https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/logo_white_fe6da1ec.png"]];
 }
 
 
@@ -25,5 +30,14 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark -getter
+- (UIImageView *)imageView {
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 200)];
+        [self.view addSubview:_imageView];
+    }
+    return _imageView;
+}
 
 @end
